457d32b2a4a87b4a778221c2ae0ba3e5eadfa6bf
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-09-27 13:48:17 -0500

    employee model and migration
    employee model and migration

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=5)
+    phone_number = models.CharField(max_length=10)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
 # TODO: Create an Employee model with properties required by the user stories
